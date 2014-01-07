e4d911de474d77171549650ecbce4a642c600676
singhakky03 <ankitas90@gmail.com>
2014-01-08 00:57:23 +0530

    Depot Scaffold

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Flipkart::Application.config.secret_key_base = '0224e71c2ad8c1aa1a152e849037fffaaa5b62d57fb6af697f854c41d9136a0ed31543789b28e7e495366426ac5c9bb3203bdca879a41f01198167839342818b'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Flipkart::Application.config.session_store :cookie_store, key: '_flipkart_session'
