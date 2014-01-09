d7373d62d5700e242e35d050d9e3108d52ddf0bb
singhakky03 <ankitas90@gmail.com>
2014-01-09 14:08:05 +0530

    Validation!

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,2 +1,8 @@
 class Product < ActiveRecord::Base
+
+	validates :title, uniqueness: true
+	validates :title, :description, :image_url, presence: true
+	validates :price, numericality: {greater_than_or_equal_to: 0.01}
+	validates :image_url, allow_blank: true, format: { with: %r{\.gif|jpg|png}$i, 
+														message: "must be a URL gor GIF,JPG,PNG image"}
 end
